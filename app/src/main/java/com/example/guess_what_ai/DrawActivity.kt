8c3247cb364a2589fdc023158570bb4080cf61e2
dadamymy <57365403+dadamymy@users.noreply.github.com>
2025-11-08 00:14:29 +0800

    Add files via upload
    inital all thing

---
+++ app/src/main/java/com/example/guess_what_ai/DrawActivity.kt
@@ -0,0 +1,23 @@
+package com.example.guess_what_ai
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.guess_what_ai.databinding.ActivityMainBinding
+
+class DrawActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/guess_what_ai/StartActivity.kt
@@ -0,0 +1,27 @@
+package com.example.guess_what_ai
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.guess_what_ai.databinding.ActivityMainBinding
+
+class StartActivity : AppCompatActivity(), View.OnClickListener{
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val btnStart: Button = binding.btnStart
+        btnStart.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == binding.btnStart.id){
+
+        }
+    }
+}
