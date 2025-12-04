0c1f43d17e49a2242c37181511711adde65fc270
honda <hondachen9@gmail.com>
2025-12-04 13:28:02 +0800

    [modify] 新增兩個Gemini api必要的Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    // Add the dependency for the Firebase AI Logic library When using the BoM,
+    // you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-ai")
 }
